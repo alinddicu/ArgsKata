d05ae60f6708642d9e98c1ba8672dfc4ff1db6b8
Alin Dicu <alinddicu@yahoo.com>
2018-07-08 17:47:59 +0200

    ArgsKata solution init

---
+++ ArgsKata/ArgsKata/ArgsKata.Tests/ArgsTests.cs
@@ -0,0 +1,13 @@
+﻿namespace ArgsKata.Tests
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class ArgsTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}

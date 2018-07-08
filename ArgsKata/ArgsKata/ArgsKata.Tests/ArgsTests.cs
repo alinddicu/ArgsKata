9c2cd8b424e1ba82ce5b997a9df4e98884ea59e4
Alin Dicu <alinddicu@yahoo.com>
2018-07-08 18:14:02 +0200

    1st failing test

--- ArgsKata/ArgsKata/ArgsKata.Tests/ArgsTests.cs
+++ ArgsKata/ArgsKata/ArgsKata.Tests/ArgsTests.cs
@@ -1,13 +1,22 @@
 ﻿namespace ArgsKata.Tests
 {
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using NFluent;
 
 	[TestClass]
 	public class ArgsTests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void TestArgs1()
 		{
+			var args = new Args("l,p#,d*", new[] { "-l", "-p", "8080", "-d", "/usr/logs" });
+			var l = args.GetValue<bool>("l");
+			var p = args.GetValue<int>("p");
+			var d = args.GetValue<string>("d");
+
+			Check.That(l).IsTrue();
+			Check.That(p).IsInstanceOf<int>().And.IsEqualTo(8080);
+			Check.That(d).IsInstanceOf<string>().And.IsEqualTo("/usr/logs");
 		}
 	}
 }
--- ArgsKata/ArgsKata/ArgsKata/Args.cs
+++ ArgsKata/ArgsKata/ArgsKata/Args.cs
@@ -1,6 +1,21 @@
 ﻿namespace ArgsKata
 {
-    public class Args
+	using System.Net.Http.Headers;
+
+	public class Args
     {
+	    private readonly string _schema;
+		private readonly string[] _arguments;
+
+	    public Args(string schema, string[] arguments)
+	    {
+		    _schema = schema;
+		    _arguments = arguments;
+	    }
+
+	    public TValue GetValue<TValue>(string argName)
+	    {
+		    throw new System.NotImplementedException();
+	    }
     }
 }
